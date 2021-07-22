9990601c594d9151009fc459dc1879e62fb8c79c
Andrew <antiuleanu2001@gmail.com>
2021-07-22 03:17:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+//  let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a:50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// console.log(name);
+
+// var name = 'Ivan';
+
+// name =  'Alex';
+
+// {
+//     var result = 50;
+
+// }console.log(result);
+
+// console.log('string'*9);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMaried: false
+// };
+
+// // console.log(obj.name);
+//  console.log(obj["name"]);
+
+//  let arr = ['plum.png', 'orange.jpeg', 6, 'apple.bmp'];
+//  console.log(arr[1]);
+// //  alert('Hello');
+// // const result = confirm("Are u creze?");
+// // console.log(result);
+// const answer = prompt("Are u 18?", "18");
+// console.log( typeof(answer) );
+
+// const answers = [];
+
+// // answers[0] = prompt('Как ваше имя?','');
+// // answers[1] = prompt('Как ваша фамилия?','');
+// // answers[2] = prompt('Сколько вам лет?','');
+
+
+// console.log(typeof(answers));
+
+// let points = [ // An array with 2 elements.
+//     {x: 0, y: 0}, // Each element is an object.
+//     {x: 1, y: 1}
+//    ];
+//    points[1].x - points[0].x;
+
+//    points.dist = function() { // Define a method to compute distance between points
+//     let p1 = this[0]; // First element of array we're invoked on
+//     let p2 = this[1]; // Second element of the "this" object
+//     let a = p2.x-p1.x; // Difference in x coordinates
+//     let b = p2.y-p1.y; // Difference in y coordinates
+//     returnMath.sqrt(a*a + // The Pythagorean theorem
+//     b*b)); // Math.sqrt() computes the square root
+//    };
+//    points.dist(); 
+//    let primes = [2, 3, 5, 7]; // An array of 4 values, delimited with [ and ].
+// primes[0] // => 2: the first element (index 0) of the array.
+// primes.length // => 4: how many elements in the array.
+// primes[primes.length-1] // => 7: the last element of the array.
+// primes[4] = 9; // Add a new element by assignment.
+// primes[4] = 11; // Or alter an existing element by assignment.
+// let empty = []; // [] is an empty array with no elements.
+// empty.length
+
+const catgory = 'toys';
+
+console.log(`https://someurl.com/${catgory}/5 `);
+const user = "Ivan";
+alert(`Hello, ${user}`);
